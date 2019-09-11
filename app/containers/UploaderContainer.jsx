915ba5d17a5662dfe70ab33db1109eff7dcadca5
Glitch (starter-react) <none>
2019-09-11 22:55:50 +0000

    💘🏗 Checkpoint ./app/containers/UploaderContainer.jsx:6678797/1561

--- app/containers/UploaderContainer.jsx
+++ app/containers/UploaderContainer.jsx
@@ -3,6 +3,8 @@ const React = require('react');
 const UploadForm = require('../components/UploadForm.jsx');
 const UploadProgress = require('../components/UploadProgress.jsx');
 
+const UPLOAD_BUCKET = 'https://skylight-react-interview-project.s3.amazonaws.com'
+
 class UploaderContainer extends React.Component {
   constructor(props) {
     super(props)
@@ -22,6 +24,30 @@ class UploaderContainer extends React.Component {
   startUpload(file) {
     // Start the file upload process
     // for an individual file
+    console.log(file)
+    let xhr = new XMLHttpRequest()
+    let uploadUrl = `${ UPLOAD_BUCKET }/${ file.name }`
+    xhr.open('PUT', uploadUrl)
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) { 
+          console.log(file.sourceURL)
+          console.log('reached 100')
+        }
+      }
+    }      
+    xhr.upload.onprogress = function (e) {        
+      var percentComplete = Math.ceil((e.loaded / e.total) * 100); 
+      console.log(file.sourceURL)
+      console.log(percentComplete)
+    }      
+    xhr.setRequestHeader('Content-Type', file.mime)
+    xhr.ontimeout = (e) => {     
+      xhr.abort()
+      console.log(file.sourceURL)
+      console.log('failed')
+    }
+    xhr.send({ uri: file.sourceURL, type: file.mime })
   }
   
   render() {
