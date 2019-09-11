88df09c65a470067890ff0611a350a619fb6fe28
Glitch (starter-react) <none>
2019-09-11 21:34:49 +0000

    🏓💅 Checkpoint ./app/containers/UploaderContainer.jsx:6678797/1737

--- app/containers/UploaderContainer.jsx
+++ app/containers/UploaderContainer.jsx
@@ -1,36 +1,11 @@
 const React = require('react');
-const UnorderedList = require('../components/UnorderedList');
 
-const dependenciesArray = [
-  'express - middleware for the node server',
-  'react - for generating the views of the app',
-  'react-dom - powers the rendering of elements to the DOM, typically paired with React',
-  'webpack - for bundling all the javascript',
-  'webpack-cli - command line support for webpack',
-  'jsx-loader - allows webpack to load jsx files'
-];
+const Uploader = require('../components/Uploader.jsx')
 
-const componentsMade = [
-  'HelloWorld - which is the view you are seeing now!',
-  'UnorderedList - which takes an array of "items" and returns a <ul> element with <li>, elements of each of those items within it',
-];
-
-/* the main page for the index route of this app */
-const UploaderContainer = function() {
-  return (
-    <div>
-      <h1>Hi world</h1>
-
-      <p>This is a starter <a href="http://glitch.com">Glitch</a> app for React! It uses 
-        only a few dependencies to get you started on working with React:</p>
-
-      <UnorderedList items={dependenciesArray} />
-
-      <p>Look in <code>app/components/</code> for two example components:</p>
-
-      <UnorderedList items={componentsMade} />
-    </div>
-  );
+class UploaderContainer extends React.Component {
+  render() {
+    return <Uploader />;
+  }
 }
 
 module.exports = UploaderContainer;
